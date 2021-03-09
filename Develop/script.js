b9fa24977edbca482f8947d16c482a3ffacf0c77
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-03-08 20:34:10 -0600

    Adding info to readme, adding js information

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,70 @@
 // Set up hours and times from moment 
-
+var myDay = [
+    {
+        id: "0",
+        hour: "09",
+        time: "09",
+        ampm: "am",
+        reminder: ""
+    },
+    {
+        id: "1",
+        hour: "10",
+        time: "10",
+        ampm: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
+        hour: "11",
+        time: "11",
+        ampm: "am",
+        reminder: ""
+    },
+    {
+        id: "3",
+        hour: "12",
+        time: "12",
+        ampm: "pm",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "01",
+        time: "13",
+        ampm: "pm",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "02",
+        time: "14",
+        ampm: "pm",
+        reminder: ""
+    },
+    {
+        id: "6",
+        hour: "03",
+        time: "15",
+        ampm: "pm",
+        reminder: ""
+    },
+    {
+        id: "7",
+        hour: "04",
+        time: "16",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "8",
+        hour: "05",
+        time: "17",
+        ampm: "pm",
+        reminder: ""
+    },
+    
+]
 
 // Create function for the date 
 
@@ -14,3 +79,4 @@ function saveReminders() {
 }
 
 // Add Font Awesome icon
+
